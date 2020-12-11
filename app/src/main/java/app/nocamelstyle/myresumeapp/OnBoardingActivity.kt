5ba926d6ec433b3adfec7314e9f5ca7f19499911
Karmanckik <andreysxkormachenko@gmail.com>
2020-12-11 06:45:04 +0300

    Initial commit

---
+++ app/src/main/java/app/nocamelstyle/myresumeapp/OnBoardingActivity.kt
@@ -0,0 +1,11 @@
+package app.nocamelstyle.myresumeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
---
+++ app/src/main/java/app/nocamelstyle/myresumeapp/SplashActivity.kt
@@ -0,0 +1,11 @@
+package app.nocamelstyle.myresumeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
